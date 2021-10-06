4b146f1c6aab296990d7571d304de934a3291b59
Ana Flávia Souza <aflavia17_@hotmail.com>
2021-10-05 21:57:37 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
-import { createApp } from 'vue'
+import {
+    createApp
+} from 'vue'
 import App from './App.vue'
+
+import 'spectre.css/dist/spectre.min.css'
+import 'spectre.css/dist/spectre-icons.min.css'
 
 createApp(App).mount('#app')
