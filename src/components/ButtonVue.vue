85d002ec2a972afa7e22a150cd3446a2551ce4de
add-flav1s <aflavia17_@hotmail.com>
2021-10-05 21:32:22 -0300

    Criando nosso primeiro componente em vue com algumas funcionalidades.

---
+++ src/components/ButtonVue.vue
@@ -0,0 +1,19 @@
+<template>
+    <button @click="click">Cliquei {{ count }} vezes.</button>
+</template>
+
+<script>
+export default {
+    name: "ButtonVue",
+    data() {
+        return {
+            count: 0,
+        };
+    },
+    methods: {
+        click() {
+            this.count++;
+        },
+    },
+};
+</script>
